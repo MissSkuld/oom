2889bedef4fcdec7939f09a7dfa282c898a21cf3
MissSkuld <missskuld@gmail.com>
2016-02-11 20:13:41 +0100

    initial commit

---
+++ tasks/Task1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "Hello world!";
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"{hello}");
+        }
+    }
+}
